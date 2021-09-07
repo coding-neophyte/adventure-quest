5e915447532372332dda748604e0ad55f8532eb8
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-07 14:07:25 -0700

    Added planning comments  for todays items

---
+++ results/results.js
@@ -0,0 +1,10 @@
+// ***********TODO*****************
+// 1) Evaluation of result on results page
+
+// get data from local storage, check hp and rupees
+// make functions with if statements to determine results 
+
+// 2) Results Page design and information display
+
+// hardcode a p tag and inject
+// ***********************************
