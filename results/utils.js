149377342da37b949d72dff1bc0074db035e99b3
EmilyDSarani <emily.sarani@gmail.com>
2021-09-07 16:46:10 -0500

    added to questjs and utilsjs

---
+++ results/utils.js
@@ -0,0 +1,22 @@
+export const aliveRupeeMessages = {
+    broke: `you'll be breaking pots for the rest of your life. Just you and Branchy`,
+    comfortable: `no more wooden sword for you, HI-YAH!`,
+    rich: `you could buy your own boat instead of building one`
+
+
+};
+
+export const deadRupeeMessages = {
+    broke: `you left nothing for your boat`,
+    comfortable: `you left enough money for your boat to find a branch`,
+    rich: `you left enough money for your boat to buy a boat`,
+
+
+};
+
+export const hpMessages = {
+    dead: `sea buriel, someone shot a flaming arrow for your boat. Press F in chat.`,
+    frail: `but, did you die?`,
+    healthy: `you lived, but had no flair. You literally made all the right choices. How boring.`,
+
+};
