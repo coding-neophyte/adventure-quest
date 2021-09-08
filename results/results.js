9c290c2cf07d84f250f7b18639b2f3822ff40130
Julius Franklin <jjfranklin88@outlook.com>
2021-09-07 20:06:29 -0400

    comment out

--- results/results.js
+++ results/results.js
@@ -1,35 +1,35 @@
-import { getCharacter } from '../local-storage.js';
-import { aliveRupeeMessages, deadRupeeMessages, hpMessages } from './utils.js';
+// import { getCharacter } from '../local-storage.js';
+// import { aliveRupeeMessages, deadRupeeMessages, hpMessages } from './utils.js';
 
 
 
-// ***********TODO*****************
-// 1) Evaluation of result on results page
+// // ***********TODO*****************
+// // 1) Evaluation of result on results page
 
-// get data from local storage, check hp and rupees
-// make functions with if statements to determine results
+// // get data from local storage, check hp and rupees
+// // make functions with if statements to determine results
 
-// 2) Results Page design and information display
+// // 2) Results Page design and information display
 
-// hardcode a p tag and inject
-// **********************************
+// // hardcode a p tag and inject
+// // **********************************
 
-const user = getCharacter();
-function healthyLink(user) {
-    if (user.hp <= 0) return 'dead';
-    if (user.hp < 2) return 'frail';
-    return 'healthy';
-}
+// const user = getCharacter();
+// function healthyLink(user) {
+//     if (user.hp <= 0) return 'dead';
+//     if (user.hp < 2) return 'frail';
+//     return 'healthy';
+// }
 
-function wealthyLink(user) {
-    if (user.rupees <= 0) return 'broke';
-    if (user.rupees < 100) return 'comfortable';
-    return 'rich';
-}
+// function wealthyLink(user) {
+//     if (user.rupees <= 0) return 'broke';
+//     if (user.rupees < 100) return 'comfortable';
+//     return 'rich';
+// }
 
-// // const user = getCharacter();
-// const healthy = healthyLink(user);
-// const wealthy = wealthyLink(user);
+// // // const user = getCharacter();
+// // const healthy = healthyLink(user);
+// // const wealthy = wealthyLink(user);
 
-// healthy === 'dead' ? inject deadRupeeMessages: inject aliveRupeeMessages;
-// healthy
+// // healthy === 'dead' ? inject deadRupeeMessages: inject aliveRupeeMessages;
+// // healthy
